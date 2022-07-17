fe4ac9b0f4da4b0f8b49ff2e174624d9267ba220
tamaki05 <kx0c0xk@gmail.com>
2022-07-17 06:32:31 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :customers
+  root :to =>"public/homes#top"
+  get "about"=>"public/homes#about"
+
+  get 'cart_items' => 'public/cart_items#index'
+  get 'customers/:id' => 'public/customers#show'
+  get 'customers/:id/edit' => 'public/customers#edit'
+  get 'items' => 'public/items#index'
+  get 'items/:id' => 'public/items#show'
+
+  namespace :admin do
+    resources :customers, only: [:index,:show,:edit]
+    resources :genres, only: [:index,:edit]
+
+    get ""=>"homes#top"
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
