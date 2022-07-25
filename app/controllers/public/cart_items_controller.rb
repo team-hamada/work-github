630e00e311ec695ccb006bd91d8cfe4e19068c49
Makoto Hamada <ec2-user@ip-172-31-33-80.ec2.internal>
2022-07-25 04:13:23 +0000

    (マコト)cart_itemsの[カートに入れる]が出来ていない状態

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,39 @@
 class Public::CartItemsController < ApplicationController
+  
   def index
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  def destroy_all
+    @cart =CartItem.where(customer_id: current_customer.id)
+    @total = 0
   end
 
   def create
+   @cart_item = CartItem.new(cart_params)
+   @cart_item.customer_id=current_customer.id
+   @cart_item.save
+   redirect_to public_cart_items_path
   end
+
+  def update
+    cart_item=CartItem.find(params[:id])
+    cart_item.update(amount: cart_params[:amount].to_i)
+    redirect_to public_cart_items_path
+  end
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
+
+  def destroy_all
+    cart_item = CartItem.where(customer_id: current_customer.id)
+    cart_item.destroy_all
+    redirect_to cart_items_path
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart_item).permit(:item_id,:amount)
+  end
+
 end
