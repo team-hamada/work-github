5be80dc471883f5dd5fc4013a3ba3d7bd9d75f3b
EC2 Default User <ec2-user@ip-172-31-15-216.ec2.internal>
2022-07-25 04:13:13 +0000

    (dai)public-orderの途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,14 +1,29 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+    @customer = current_customer
   end
 
   def comfirm
+    @order = Order.new(order_params)
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+    elsif params[:order][:select_address] == "1"
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+    end
+    @cart_items = current_customer.cart_items.all
   end
 
   def complete
   end
 
   def create
+    @order=Order.new(order_params)
   end
 
   def index
@@ -16,4 +31,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:payment_method,:postal_code, :address, :name)
+  end
+  
 end
