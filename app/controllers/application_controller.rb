edb9f160ae90158ccb49a544b198605dc6d1e30c
tamaki05 <kx0c0xk@gmail.com>
2022-07-25 06:40:33 +0000

    (たま)topとaboutのviewの更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    customers_my_page_path
+    root_path
   end
 
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   protected
