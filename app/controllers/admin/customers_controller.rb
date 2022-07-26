c73cbf53477dab4c9d5495f80fc78ebf8fd49296
4kang <106650699+4kang@users.noreply.github.com>
2022-07-26 15:35:43 +0000

    (まゆ)Admincustomereditの編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,7 +1,7 @@
 class Admin::CustomersController < ApplicationController
   def index
-    
-    @customers = Customer.all 
+
+    @customers = Customer.all
   end
 
   def show
@@ -13,12 +13,15 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
   end
-  
+
   private
-  
+
   def customer_params
-    params.require(:customer).permit(:full_name, :full_name_kana, :email, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:full_name, :full_name_kana, :email, :postal_code, :address, :telephone_number, :is_deleted, :last_name_kana, :first_name_kana, :last_name, :first_name)
   end
-  
+
 end
