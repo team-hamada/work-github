ce2dbe927194697fcacc28efd0d158e7377c1c3f
4kang <106650699+4kang@users.noreply.github.com>
2022-07-26 08:55:56 +0000

    (まゆ)admincustomer,show,index編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,24 @@
 class Admin::CustomersController < ApplicationController
   def index
+    
+    @customers = Customer.all 
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:full_name, :full_name_kana, :email, :postal_code, :address, :telephone_number)
+  end
+  
 end
